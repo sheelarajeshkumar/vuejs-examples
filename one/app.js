3669b49314e22d9a564049fa8e7f458aadd9fcb9
rksheela <rajesh.sheela@oracle.com>
2021-06-16 19:28:57 +0530

    first commit

---
+++ one/app.js
@@ -0,0 +1,15 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            goals: [],
+            enterValue: ''
+        };
+    },
+    methods: {
+        addGoal(){
+            this.goals.push(this.enterValue);
+            this.enterValue = '';
+        }
+    },
+}).mount("#app");
+
